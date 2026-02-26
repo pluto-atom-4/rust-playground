3772b50c788d8372dfe1cd129186a5ad7324e289
Akai Kaede <genko2@hotmail.com>
2026-02-25 21:13:15 -0800

    implement number guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::cmp::Ordering;
+use std::io;
+use rand::prelude::thread_rng;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    let mut rng = thread_rng();
+    let secret_number = rng.gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                return;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
