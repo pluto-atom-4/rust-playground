f5fc1a52a5625ddc9054c7212ce068464aee5a6b
Akai Kaede <genko2@hotmail.com>
2026-02-27 20:02:35 -0800

    demonstrate variable shadowing and scope in Rust

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    let mut x = 5;
-    println!("The value of x is: {}", x);
-    x = 7;
-    println!("The value of x is: {}", x);
+    let x = 5;
+
+    let x = x + 1;
+
+    // shadowing allows us to reuse the same variable name, but it creates a new variable that shadows the previous one.
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+println!("The value of x is: {}", x);
+
 }
