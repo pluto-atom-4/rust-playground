96c13e5960d55116b3c88ba8bf2ce243d102510d
Akai Kaede <genko2@hotmail.com>
2026-02-27 20:05:51 -0800

    Problem "expected `&str`, found `usize`"

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,6 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
-    let x = 5;
-
-    let x = x + 1;
-
-    // shadowing allows us to reuse the same variable name, but it creates a new variable that shadows the previous one.
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
-println!("The value of x is: {}", x);
-
+    let spaces = "    ";
+    spaces = spaces.len();
 }
