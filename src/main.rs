68dfd684bd30ed62b364660c93cb469a117cb10d
Akai Kaede <genko2@hotmail.com>
2026-02-27 20:00:59 -0800

    add constant for three hours in seconds

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
+    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 7;
