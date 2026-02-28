ffe9087bf5d6ee3133242c257ea6c48b1d2ed306
Akai Kaede <genko2@hotmail.com>
2026-02-27 19:59:51 -0800

    make variable mutable to allow reassignment

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("The value of x is: {}", x);
     x = 7;
     println!("The value of x is: {}", x);
