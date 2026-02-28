cd9b3db366d1e5ca49927d24bb668b90615fb4c6
Akai Kaede <genko2@hotmail.com>
2026-02-27 19:59:28 -0800

    Problem to update immutable variable.

--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,8 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
-
-    let mut rng = thread_rng();
-    let secret_number = rng.gen_range(1..=100);
-
-    loop {
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please enter a valid number!");
-                return;
-            }
-        };
-
-        println!("You guessed: {guess}");
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    let x = 5;
+    println!("The value of x is: {}", x);
+    x = 7;
+    println!("The value of x is: {}", x);
 }
