c5760aef9504df8281e2946a47fbea3c3764ac8d
Akai Kaede <genko2@hotmail.com>
2026-03-11 15:18:01 -0700

    array example and demonstrate element access in Rust

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,6 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
-    let tup: (i32, f64, u8) = (500, 6.4, 1);
-
-    let (_x, y, _z) = tup;
-
-    print!("The value of y is: {}", y);
+   let a = [1,2,3,4,5];
+    print!("The second element of the array is: {}", a[1]);
 }
