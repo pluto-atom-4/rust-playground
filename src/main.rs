7cec50fdc052be51ddc07dbeba6a03da62232b3c
Akai Kaede <genko2@hotmail.com>
2026-03-12 19:14:01 -0700

    add user input for element access in Rust

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,22 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
-    let a= [3; 5];
+    let a = [1,2,3,4,5];
 
-    println!("The array is: {:?}", a); // The array is: [3, 3, 3, 3, 3]
+    println!("Please enter an array index.");
+
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered wa not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is: {element}");
 }
