90f25638bd73c360bda25bcf10ce0a56e31af808
Akai Kaede <genko2@hotmail.com>
2026-03-12 19:07:38 -0700

    array initialization and simplify element access in Rust

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,7 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
-    let  a = [1,2,3,4,5];
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
 
-    println!("The first element of the array is: {}", a[0]);
-    println!("The second element of the array is: {}", a[1]);
-    println!("The third element of the array is: {}", a[2]);
-    println!("The fourth element of the array is: {}", a[3]);
-    println!("The fifth element of the array is: {}", a[4]);
+    println!("The array is: {:?}", a);
 }
