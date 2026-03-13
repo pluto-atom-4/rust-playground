48538f09ba235260a775345e418e6eea2e381e89
Akai Kaede <genko2@hotmail.com>
2026-03-12 19:02:03 -0700

    add tuple example and demonstrate destructuring in Rust

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
-   let a = [1,2,3,4,5];
-    print!("The second element of the array is: {}", a[1]);
+   let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+    println!("The value of z is: {}", z);
 }
