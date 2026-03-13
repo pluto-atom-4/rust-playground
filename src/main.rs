9820481669828b81017db6c5bb464e1c8d659f77
Akai Kaede <genko2@hotmail.com>
2026-03-12 19:04:16 -0700

    refactor tuple example to use direct element access in Rust

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,13 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
-   let tup: (i32, f64, u8) = (500, 6.4, 1);
-    let (x, y, z) = tup;
-    println!("The value of x is: {}", x);
-    println!("The value of y is: {}", y);
-    println!("The value of z is: {}", z);
+    let tup = (500, 6.4, 1);
+
+    let five_hundred = tup.0;
+    let six_point_four = tup.1;
+    let one  = tup.2;
+
+    println!("The value of five_hundred is: {}", five_hundred);
+    println!("The value of six_point_four is: {}", six_point_four);
+    println!("The value of one is: {}", one);
 }
