50748201ea962091a8db7418c31bc8bb786478c7
Akai Kaede <genko2@hotmail.com>
2026-03-15 10:44:13 -0700

    the example of the error trying to set the (let variable name = xxx)

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
-    let y = 6;
+    let x = (let y = 6); // error as a statement does not return values,
+    // whereas C and Ruby, where assignment returns the value of the assignment :)
 }
