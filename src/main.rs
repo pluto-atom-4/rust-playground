a6d4cadc7497512a18d31cc27f77fa7725bd6a33
Akai Kaede <genko2@hotmail.com>
2026-03-15 10:40:23 -0700

    the first step to practice statements and expressions

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,5 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
-    println!("Hello, world!");
-
-    another_function(5);
-}
-
-fn another_function(x: i32) {
-    println!("The value of x is: {x}");
+    let y = 6;
 }
