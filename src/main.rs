92609008a9848e379b822a9a4a4da8955313ed4b
Akai Kaede <genko2@hotmail.com>
2026-03-15 10:47:56 -0700

    example to demonstrate block expressions and value returns

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,11 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
-    let x = (let y = 6); // error as a statement does not return values,
-    // whereas C and Ruby, where assignment returns the value of the assignment :)
+    let y = {
+        let x = 3;
+        x + 1    // The value of x is not accessible here, but the value of y is still 4
+        // no semicolon at the end of this line, so this expression will be returned as the value of y
+    };
+
+    println!("The value of y is {y}");
 }
