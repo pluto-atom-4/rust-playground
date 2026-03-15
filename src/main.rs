c2745ab8ab90fd5fb38dc5db31dfb12e82f5e5ab
Akai Kaede <genko2@hotmail.com>
2026-03-15 10:34:31 -0700

    add another_function firstly.

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,11 @@ use rand::prelude::thread_rng;
 use rand::Rng;
 
 fn main() {
-    let a = [1,2,3,4,5];
+    println!("Hello, world!");
 
-    println!("Please enter an array index.");
+    another_function();
+}
 
-    let mut index = String::new();
-
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
-
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered wa not a number");
-
-    let element = a[index];
-
-    println!("The value of the element at index {index} is: {element}");
+fn another_function() {
+    println!("Another functions.");
 }
