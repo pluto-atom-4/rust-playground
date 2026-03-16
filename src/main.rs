72f6fb312be941e0374b76d2e6e2fe4cdc711ade
Akai Kaede <genko2@hotmail.com>
2026-03-15 20:03:09 -0700

    example a conditional assignment for number and simplify output

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,8 @@ use rand::Rng;
 
 
 fn main() {
-    let number = 6;
+    let condition = true;
+    let number = if condition { 5 } else { 6 }; // no semicolon at the end of the line, otherwise it will be treated as a statement and not an expression
 
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is dvisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
-    } else {
-        println!("number is not divisible by 4, 3, or 2");
-    }
+    println!("The value of number is: {number}");
 }
