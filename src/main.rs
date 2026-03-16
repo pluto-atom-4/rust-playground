a41e2c6c400a08aabe5c7ead0ac2d5c3d1a6e787
Akai Kaede <genko2@hotmail.com>
2026-03-15 19:48:35 -0700

    add lucky_number variable to main function

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,5 @@ use rand::Rng;
 
 
 fn main() {
-    let x = plus_one(5);
-
-    println!("The value of x is: {x}");
-
-}
-fn plus_one(x: i32) -> i32 {
-    x + 1
+    let lucky_number = 7; // I'm, feeling lucky today
 }
