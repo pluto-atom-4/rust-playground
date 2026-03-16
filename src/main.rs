aa16a5fa413337b6c35442a6b5609b789f850484
Akai Kaede <genko2@hotmail.com>
2026-03-15 19:57:44 -0700

    explicitly provide the conditional flow in main function to check for non-zero number

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use rand::Rng;
 
 fn main() {
     let number = 3;
-    if number { // This will cause a compile-time error because `number` is an integer, not a boolean.
-        println!("condition was three");
+    if number != 0 { // explicitly check for inequality to zero
+        println!("condition was something other than zero");
     }
 }
