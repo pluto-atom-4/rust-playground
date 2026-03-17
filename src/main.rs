0d26c4c288fc503310a00b478afd424d18963d57
Akai Kaede <genko2@hotmail.com>
2026-03-16 18:46:21 -0700

    Example to loop using iterator

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,8 @@ use rand::Rng;
 
 fn main() {
     let a = [10, 20, 30, 40, 50];
-    let mut index = 0;
 
-    while index < 5 {
-        println!("the value is: {}", a[index]);
-
-        index += 1;
+    for element in a {
+        println!("the value is: {element}");
     }
 }
