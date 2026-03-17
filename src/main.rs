38e303ca3191028663f4f8c5ca69d5e34c3bd375
Akai Kaede <genko2@hotmail.com>
2026-03-16 18:47:37 -0700

    Example countdown loop with liftoff message

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,8 @@ use rand::Rng;
 
 
 fn main() {
-    let a = [10, 20, 30, 40, 50];
-
-    for element in a {
-        println!("the value is: {element}");
+    for number in (1..4).rev() {
+        println!("{number}!");
     }
+    println!("LIFTOFF!!!");
 }
