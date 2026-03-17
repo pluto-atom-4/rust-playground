16162d201e784669c9a665281d0e89acc29ebaac
Akai Kaede <genko2@hotmail.com>
2026-03-16 18:31:23 -0700

    Example an infinite loop in main function

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,7 @@ use rand::Rng;
 
 
 fn main() {
-    let condition = true;
-    let number = if condition { 5 } else { "six" }; // this will cause a compile error because the types of the two branches are different
-
-    println!("The value of number is: {number}");
+    loop {
+        println!("again!");
+    }
 }
