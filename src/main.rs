cdd8d44e87a5f56d9f0fbbe5e55b24fd0433a898
Akai Kaede <genko2@hotmail.com>
2026-03-30 14:56:56 -0700

    Example string ownership issue in main function

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use rand::Rng;
 
 
 fn main() {
-    let mut s = String::from("hello");
-    s.push_str(", world!");
-    println!("{s}");
+    let s1 = String::from("hello");
+    let s2 = s1;
+    
+    println!("{s1}, world!");  // Rust consider `s1` as no longer valid.
 }
