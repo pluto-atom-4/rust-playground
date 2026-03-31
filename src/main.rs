6e0b8c18bbe94bbeae3e56ae88511e0eadb1050d
Akai Kaede <genko2@hotmail.com>
2026-03-30 18:31:46 -0700

    demonstrate ownership and copying with examples

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,23 @@
 
 
 fn main() {
-    let x = 5;
-    let y = x;
+    let s = String::from ("hello");  // s comes into scope
 
-    println!("x = {x}, y = {y}"); // x is still valid and wasn't moved into y.
-}
+    takes_ownership(s);  // s's value moves into the function...
+                         // ... and so it is no longer valid here
+    let x = 5;      // x comes into scope
+
+    makes_copy(x);       // Because i32 implements the Copy trait,
+                         // x does NOT move into the function,
+                         // so it's okay to use x afterward.
+}  // Here, x goes out of scope, then s. However, because s's value was moved,
+   // nothing special happens.
+
+fn takes_ownership(some_string: String) {  // some_string comes into scope
+    println!("{some_string}");
+} // Here, some_string goes out of scope and `drop` is called. the backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) {   // some_integer comes into scope
+    println!("{some_integer}");
+} // Here, some_integer goes out of scope. Nothing special happens.
